a91bf0d77319d215099eccdf5875f08e59693efa
sabari-D-raj <shabariraj9@gmail.com>
2026-06-23 12:09:53 +0530

    secon commit

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import cv2
 import mediapipe as mp
 import pyautogui
 cap=cv2.VideoCapture(0)
+screen_w,screen_h=pyautogui.size()
+clicked=False
 mp_hands=mp.solutions.hands
 mp_draw=mp.solutions.drawing_utils
 hands=mp_hands.Hands(static_image_mode=False,
                      max_num_hands=1,
                      min_detection_confidence=0.7,
-                     min_tracking_confidence=0.7,
+                     min_tracking_confidence=0.7
                      )
 while True:
     succes,frame=cap.read()
@@ -17,6 +19,25 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmark in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame,hand_landmark,mp_hands.HAND_CONNECTIONS)
+
+        x=int(hand_landmark.landmark[8].x*w)
+        y=int(hand_landmark.landmark[8].y*h)
+        screem_x=screen_w/w*x
+        screen_y=screen_h/h*y
+        pyautogui.moveTo(screem_x,screen_y)
+        thumb_x=(hand_landmark.landmark[4].x)
+        thumb_y=(hand_landmark.landmark[4].y)
+        index_x=(hand_landmark.landmark[8].x)
+        index_y=(hand_landmark.landmark[8].y)
+        distance=((thumb_x-index_x)**2+(thumb_y-index_y)**2)**0.5
+        print(distance)
+        if distance <0.08:
+            if not clicked:
+                pyautogui.click()
+                print("clicked")
+                clicked=True
+        else:
+            clicked=False
     if not succes:
         print("failed not working")
         break
